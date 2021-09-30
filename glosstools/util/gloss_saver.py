2772b03deb9f69df090994a0a4109e42368d8680
Retr0327 <48786838+Retr0327@users.noreply.github.com>
2021-09-30 23:31:37 +0800

    Added gloss_saver.py

---
+++ glosstools/util/gloss_saver.py
@@ -0,0 +1,45 @@
+from pathlib import Path 
+from dataclasses import dataclass
+
+
+@dataclass
+class GlossSaver:
+    """
+    The GlossSaver object saves the data to a txt file on the desktop.
+    """
+    file_name: str
+    data: str
+
+    def __post_init__(self):
+        self.desktop = Path.home() / "Desktop"
+
+    def create_folder(self) -> Path:
+        """The create_folder method creates the `gloss_replaced` folder.
+
+        Returns:
+            a folder path
+        """
+        dir_name = "gloss_replaced" 
+        new_dir = Path(self.desktop, dir_name)
+        new_dir.mkdir(parents=True, exist_ok=True)
+        return new_dir
+
+    def write_txt(self, path: Path):
+        """The write_txt method writes the data to a txt file
+
+        Args:
+            path (Path): the file path
+        """
+        with open(path / self.file_name, "w", encoding="utf-8") as f:
+            f.writelines(self.data)
+
+    def save(self):
+        """The save method saves the data to the desktop.
+        """
+        save_folder = self.desktop / 'gloss_replaced'
+        is_exist = save_folder.exists()
+        if not is_exist:
+            new_dir = self.create_folder()
+            self.write_txt(new_dir)
+
+        self.write_txt(save_folder)
